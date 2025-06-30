a549da554170b8afc629d04dca02574b93447f7f
jingyang26 <jingyanggohj2648@gmail.com>
2025-06-30 09:25:40 +0800

    update code

--- obj/Debug/net9.0/FYP1System.AssemblyInfo.cs
+++ obj/Debug/net9.0/FYP1System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FYP1System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90f7791e1a05f777499330f0198153de4b1d807f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c6de58f90b0989e9109fe3cb656c77670aa750")]
 [assembly: System.Reflection.AssemblyProductAttribute("FYP1System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FYP1System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
